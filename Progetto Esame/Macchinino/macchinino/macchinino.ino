0e56d4c9067be1d217fc5b96860932907f49e86c
marcocola2001 <cola01.mc@gmail.com>
2023-07-04 12:47:02 +0200

    Inizio assemblaggio + proseguimento scrittura codice

--- Progetto Esame/Macchinino/macchinino/macchinino.ino
+++ Progetto Esame/Macchinino/macchinino/macchinino.ino
@@ -4,49 +4,48 @@
 #include <NewPing.h>
 Servo myservo;
 
-int SPEED = 180;
-
 AF_DCMotor motor1(1, MOTOR12_1KHZ); 
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
-AF_DCMotor motor3(3, MOTOR12_1KHZ); 
-AF_DCMotor motor4(4, MOTOR12_1KHZ);
-
+AF_DCMotor motor3(3, MOTOR34_1KHZ); 
+AF_DCMotor motor4(4, MOTOR34_1KHZ);
 
 NewPing sonar(A4, A5, 100); //Triger, ECHO, Max distanza in cm
 
 bool isFwd = false;
+int SPEED = 180;
 int BTSpeed = 180;
 
 SoftwareSerial bt(9, 10);
 
 int mode; //0: BT, 1: Schiva Ostacoli
 
-
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(9600);
+  Serial.begin(9600);
   bt.begin(9600);
 
   motor1.setSpeed(SPEED);
   motor2.setSpeed(SPEED);
   motor3.setSpeed(SPEED);
   motor4.setSpeed(SPEED);
-  myservo.attach(10);  
-  myservo.write(90); 
   
   motor1.run(RELEASE);
   motor2.run(RELEASE);
   motor3.run(RELEASE);
   motor4.run(RELEASE);
+  
+  myservo.attach(10);  
+  myservo.write(90); 
 
   mode = 0;
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   if(mode == 0){
     SPEED = BTSpeed;
     int dFront = readPing();
-    if(dFront < 30 && isFwd == true){
+    if(dFront < 50 && isFwd == true){
       stopMotors();
     }
     else if(bt.available() > 0){
@@ -88,46 +87,54 @@ void loop() {
        }
      }
   }
-
+  else if(mode == 1){
+    if(SPEED != 180){
+      SPEED = 180;
+    }
+    if(bt.available()){
+      char data = bt.read();
+      if(data == 'M'){
+        mode = 0;
+        stopMotors();
+      }
+    }
+    else{
+      int dFront = readPing();
+      if(dFront > 50){
+        goForward();
+      }
+      else{
+        stopMotors();
+        delay(500);
+        int dl = lookLeft();
+        Serial.println(dl);
+        delay(500);
+        int dr = lookRight();
+        Serial.println(dr);
+        delay(500);
+        if(dr >= dl){
+          turnRight();
+          delay(650);
+          stopMotors();
+          delay(100);
+        }
+        else {
+          turnLeft();
+          delay(650);
+          stopMotors();
+          delay(100);
+        }
+      }
+    }
+  }
 }
 
 int readPing() { 
   delay(70);
   int cm = sonar.ping_cm();
-  Serial.print(cm);
   if(cm == 0)
     cm = 100;
   return cm;
-}
-
-void goForward(){
-  isFwd = true;
-  motor1.run(BACKWARD);
-  motor2.run(BACKWARD);
-  motor3.run(FORWARD);
-  motor4.run(FORWARD);
-  for(int i = 100; i < SPEED; i+=2){
-    motor1.setSpeed(i);
-    motor2.setSpeed(i);
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);  
-    delay(5);
-  }
-}
-
-void goBackward(){
-  isFwd = false;
-  motor1.run(FORWARD);
-  motor2.run(FORWARD);
-  motor3.run(BACKWARD);
-  motor4.run(BACKWARD);
-  for(int i = 100; i < SPEED; i+=2){
-    motor1.setSpeed(i);
-    motor2.setSpeed(i);
-    motor3.setSpeed(i);
-    motor4.setSpeed(i);  
-    delay(5);
-  }
 }
 
 void turnLeft(){
@@ -148,9 +155,72 @@ void turnRight(){
   motor3.setSpeed(180);
   motor4.setSpeed(180);
   
-  motor1.run(FORWARD);
-  motor4.run(BACKWARD);
+  motor4.run(FORWARD);
+  motor1.run(BACKWARD);
 }
+
+void goForward(){     //avanti
+  isFwd = true;
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+  for(int i = 100; i < SPEED; i+=2){
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);  
+    delay(5);
+  }
+}
+
+void goBackward(){    //indietro
+  isFwd = false;
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+  for(int i = 100; i < SPEED; i+=2){
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);  
+    delay(5);
+  }
+}
+
+int lookLeft(){
+  for(int i = 90; i > 0; i-= 4){
+    myservo.write(i);
+    delay(10);
+  }
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  for(int i = 0; i < 90; i+=4){
+    myservo.write(i);
+    delay(10);
+  }
+  myservo.write(90);
+  return distance;
+}
+
+int lookRight(){
+  for(int i = 90; i < 180; i+=4){
+    myservo.write(i);
+    delay(10);
+  }
+  delay(500);
+  int distance = readPing();
+  delay(100);
+  for(int i = 180; i > 90; i-=4){
+    myservo.write(i);
+    delay(10);
+  }
+  myservo.write(90);
+  return distance;
+}
+
 
 void stopMotors(){
   motor1.run(RELEASE);
@@ -159,4 +229,3 @@ void stopMotors(){
   motor4.run(RELEASE);
   isFwd = false;
 }
-
